a6dc220e7c790afefa486417fa41381fcd9ee4fd
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-12-24 09:32:54 +0530

    -small changes

--- src/components/DisplayWeather.jsx
+++ src/components/DisplayWeather.jsx
@@ -13,7 +13,7 @@ const DisplayWeather = ({lati,long,result}) => {
             {/* <div className='w-24 p-8 shadow-2xl mx-60'>
                 <img src = {result.weather[0].icon} alt={result.weather[0].description} className="scale-150"></img>
             </div> */}
-            <h2>Our system detects that your approximate location is {result.name}</h2>
+            <h2>Our system detects that your approximate location is <span className='text-xl text-indigo-500 font-semibold'>{result.name}</span></h2>
             With Latitude: <span className='text-xl text-indigo-500 font-semibold'>{lati}</span>
             <br/>
             With Longitude: <span className='text-xl text-indigo-500 font-semibold'>{long}</span>
